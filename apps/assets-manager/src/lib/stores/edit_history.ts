c34196f5e87b91722b995e48b6121a1dcf59a032
davilivob <david.iii.chiu@gmail.com>
2024-06-12 10:16:01 +0800

    store more info in editHistory store

--- apps/assets-manager/src/lib/stores/edit_history.ts
+++ apps/assets-manager/src/lib/stores/edit_history.ts
@@ -1,13 +1,18 @@
 import { derived, writable } from 'svelte/store';
+import { type TableName } from '@/config';
 
-const editHistory = writable<string[]>([]);
+type EditInfo = { table: TableName; id: string };
+
+const editHistory = writable<EditInfo[]>([]);
 const editing = derived(editHistory, ($hist) => $hist.at(-1));
 
-function setEditing(id: string) {
-	editHistory.update((hist) => [...hist, id]);
+function setEditing(data: EditInfo) {
+	editHistory.update((hist) => [...hist, data]);
 }
 function backEditing() {
 	editHistory.update((hist) => hist.slice(0, -1));
 }
 
 export { editing, editHistory, setEditing, backEditing };
+
+export type { EditInfo };
