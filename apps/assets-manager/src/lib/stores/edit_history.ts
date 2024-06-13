4f191d41f29ebb334b734a8811f3438e163a84f9
davilivob <david.iii.chiu@gmail.com>
2024-06-13 22:46:26 +0800

    better naming convention for edit_history.ts & set default value for developing

--- apps/assets-manager/src/lib/stores/edit_history.ts
+++ apps/assets-manager/src/lib/stores/edit_history.ts
@@ -1,9 +1,11 @@
 import { derived, writable } from 'svelte/store';
 import { type TableName } from '@/config';
 
-type EditInfo = { table: TableName; id: string };
+type EditInfo = { tableName: TableName; id: string };
 
-const editHistory = writable<EditInfo[]>([]);
+const editHistory = writable<EditInfo[]>([
+	{ tableName: 'regions', id: '6612ead0-c675-454e-a8dd-6ab94f94eb81' }
+]);
 const editing = derived(editHistory, ($hist) => $hist.at(-1));
 
 function setEditing(data: EditInfo) {
