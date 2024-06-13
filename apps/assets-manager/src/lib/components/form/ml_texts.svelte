2780909fd3390fdeafa3f4792f6f52299cff1c3f
davilivob <david.iii.chiu@gmail.com>
2024-06-13 22:56:54 +0800

    Basic reactive design in ml_texts.svelte

--- apps/assets-manager/src/lib/components/form/ml_texts.svelte
+++ apps/assets-manager/src/lib/components/form/ml_texts.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { LOCALES } from '@/config';
-	import type { Tables } from '@repo/supabase';
-	import type { Props } from '@/components/form/types';
+	import type { PageData } from '../../../routes/$types';
 
-	let { data = $bindable() }: Props<Tables<'ml_texts'>> = $props();
+	type Props = { data: { row_id: string; column_name: string }; class?: string };
+	let { data, class: className }: Props = $props();
+
+	const { row_id, column_name } = data;
+
+	const mlTexts = ($page.data as PageData).ml_texts.filter(
+		(text) => text.row_id === row_id && text.column_name === column_name
+	);
+
+	let sortedMLTexts = $state(
+		LOCALES.map((locale) => {
+			return mlTexts.find((m) => m.locale === locale);
+		})
+	);
+	console.log(sortedMLTexts);
 </script>
 
-{#each LOCALES as locale}
-	{locale}
-	<input type="text" bind:value={data.value} />
-{/each}
+<div class="{className} flex flex-col text-left gap-1">
+	{#each LOCALES as locale, i}
+		{#if sortedMLTexts[i]}
+			<div class="flex flex-row gap-1">
+				{locale}
+				<input type="text" bind:value={sortedMLTexts[i].value} />
+			</div>
+		{/if}
+	{/each}
+</div>
