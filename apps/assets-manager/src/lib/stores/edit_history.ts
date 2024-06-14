ad7c44e1aa77f8bbbb1b3cff86456d97b5f39383
davilivob <david.iii.chiu@gmail.com>
2024-06-14 14:38:18 +0800

    Use "latest" version for deps which won't affect apps' run-time functionality

--- apps/assets-manager/src/lib/stores/edit_history.ts
+++ apps/assets-manager/src/lib/stores/edit_history.ts
@@ -3,14 +3,14 @@ import { type TableName } from '@/config';
 
 type EditInfo = { tableName: TableName; id: string };
 
-const editHistory = writable<EditInfo[]>([
-	{ tableName: 'regions', id: '6612ead0-c675-454e-a8dd-6ab94f94eb81' }
-]);
+const editHistory = writable<EditInfo[]>([]);
+
 const editing = derived(editHistory, ($hist) => $hist.at(-1));
 
 function setEditing(data: EditInfo) {
 	editHistory.update((hist) => [...hist, data]);
 }
+
 function backEditing() {
 	editHistory.update((hist) => hist.slice(0, -1));
 }
