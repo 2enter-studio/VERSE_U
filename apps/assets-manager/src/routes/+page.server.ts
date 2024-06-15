a04a0ee291f3ea0e5a574f5b26b12da4a2e8d415
davilivob <davilivob@gmail.com>
2024-06-15 19:55:45 +0800

    (assets manager) Create basic form actions

--- apps/assets-manager/src/routes/+page.server.ts
+++ apps/assets-manager/src/routes/+page.server.ts
@@ -1,7 +1,40 @@
-import { loadMLTexts, loadTables } from '@/server/db';
+import { createRow, loadMLTexts, loadTables } from '@/server/db';
+import type { Action, Actions } from '@sveltejs/kit';
+import { TABLE_NAMES, type TableName } from '@/config';
 
 export const load = async () => {
 	const tables = loadTables();
 	const ml_texts = await loadMLTexts();
 	return { tables, ml_texts };
 };
+
+const create: Action = async ({ request }) => {
+	const formData = await request.formData();
+	const tableName = formData.get('table') as TableName;
+
+	if (!TABLE_NAMES.includes(tableName)) {
+		return { message: 'invalid table name' };
+	}
+
+	const result = await createRow(tableName);
+	if ('error' in result) return { message: 'something went wrong' };
+	console.log(result.id);
+};
+
+const update: Action = async ({ request }) => {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+	console.log(data);
+};
+
+const remove: Action = async ({ request }) => {
+	const formData = await request.formData();
+	const data = Object.fromEntries(formData);
+	const { tableName, id } = data;
+
+	console.log(data);
+};
+
+const actions: Actions = { create, update, remove };
+
+export { actions };
