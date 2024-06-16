17caf991e6cbf0ed881878cca7f35c58a9fa0425
davilivob <david.iii.chiu@gmail.com>
2024-06-17 00:49:05 +0800

    (assets manager) utils: return sorted row name in getRowName()

--- apps/assets-manager/src/lib/index.ts
+++ apps/assets-manager/src/lib/index.ts
@@ -3,13 +3,17 @@ import { get } from 'svelte/store';
 
 import type { PageData } from '../routes/$types';
 import type { Tables } from '@repo/supabase';
-import type { TableName } from '@/config';
+import { LOCALES, type TableName } from '@/config';
 
 function getRowName(row: Tables<TableName>) {
-	const texts = (get(page).data as PageData).ml_texts
-		?.filter((ml_text) => ml_text.row_id === row.id && ml_text.column_name === 'name')
-		?.map((texts) => texts.value);
-	if (texts.length > 0) return texts.join('/');
+	const texts = (get(page).data as PageData).ml_texts?.filter(
+		(ml_text) => ml_text.row_id === row.id && ml_text.column_name === 'name'
+	);
+
+	if (texts.length > 0)
+		return LOCALES.map((locale) => texts.find((text) => text.locale === locale))
+			.map((t) => t?.value)
+			.join('/');
 	return (row as { value?: string })?.value ?? '?????????';
 }
 
