3bac84d9aa676f94617ee513afb059db61608fa7
davilivob <davilivob@gmail.com>
2024-06-16 16:00:55 +0800

    (assets manager) editor.svelte: remove unused code && add data validation before render

--- apps/assets-manager/src/routes/editor.svelte
+++ apps/assets-manager/src/routes/editor.svelte
@@ -3,12 +3,10 @@
 	import type { Tables } from '@repo/supabase';
 	import { type TableName, TABLES_INFO } from '@/config';
 
-	import { enhance } from '$app/forms';
 	import { typeOverRide } from '@repo/utils/types';
-	import { HiddenInput } from '@/components';
 
 	import Forms from '@/components/form';
-	import { SubmitBtn } from '@/components/index.js';
+	import { SubmitBtn } from '@/components';
 
 	type Props = { tableName: TableName; tableData: Tables<TableName>; class?: string };
 	let { tableName, tableData, class: className }: Props = $props();
@@ -31,62 +29,56 @@
 		}
 		if (Object.keys(result).length === 0) return null;
 		return result;
-		// return !deepEqual($state.snapshot(data), dataCopy);
 	});
 
-	$effect(() => {
-		console.log(modified);
-	});
+	// $effect(() => {
+	// 	console.log(modified);
+	// });
 
 	const returnComponent = (c: any) => typeOverRide<Component>(c);
 </script>
 
-<div class="flex flex-row items-start {className}">
-	{#if modified}
-		<SubmitBtn
-			action="?/update"
-			data={{ id: data.id, data: JSON.stringify(modified), table: tableName }}
-			icon="mingcute:save-2-line"
-			class="center-content"
-		/>
-	{/if}
-	<form
-		id="editor"
-		action="?/update"
-		method="post"
-		use:enhance
-		class="flex flex-col gap-3 items-start text-center border-l-2 border-b-2 border-white w-full p-2"
-	>
-		<HiddenInput name="data" value={JSON.stringify(modified)} />
-		<HiddenInput name="table" value={tableName} />
-
-		{#each Object.entries(metadata) as [name, content]}
-			{@const form = returnComponent(Forms[content.type])}
-			<div class="flex flex-col items-start">
-				<h2 class="text-bold">- {name}</h2>
-				{#if content.type !== 'ml_texts'}
-					<svelte:component
-						this={form}
-						bind:data={data[name]}
-						{name}
-						class="{content.readonly ? 'bg-gray-500 pointer-events-none' : ''} "
-					/>
-				{:else}
-					<svelte:component
-						this={Forms.ml_texts}
-						data={{ row_id: data.id, column_name: name }}
-						{name}
-					/>
-				{/if}
-			</div>
-		{/each}
-
-		{#if tableInfo?.reference}
-			reference
+{#if data}
+	<div class="flex flex-row items-start {className}">
+		{#if modified}
+			<SubmitBtn
+				action="?/update"
+				data={{ id: data.id, data: JSON.stringify(modified), table: tableName }}
+				icon="mingcute:save-2-line"
+				class="center-content"
+			/>
 		{/if}
+		<div
+			class="flex flex-col gap-3 items-start text-center border-l-2 border-b-2 border-white w-full p-2"
+		>
+			{#each Object.entries(metadata) as [name, content]}
+				{@const form = returnComponent(Forms[content.type])}
+				<div class="flex flex-col items-start">
+					<h2 class="text-bold">- {name}</h2>
+					{#if content.type !== 'ml_texts'}
+						<svelte:component
+							this={form}
+							bind:data={data[name]}
+							{name}
+							class="{content.readonly ? 'bg-gray-500 pointer-events-none' : ''} "
+						/>
+					{:else}
+						<svelte:component
+							this={Forms.ml_texts}
+							data={{ row_id: data.id, column_name: name }}
+							{name}
+						/>
+					{/if}
+				</div>
+			{/each}
 
-		{#if tableInfo?.storage}
-			storage
-		{/if}
-	</form>
-</div>
+			{#if tableInfo?.reference}
+				reference
+			{/if}
+
+			{#if tableInfo?.storage}
+				storage
+			{/if}
+		</div>
+	</div>
+{/if}
