3abe917c51047da4384bae6c33e18189646ad46c
davilivob <davilivob@gmail.com>
2024-06-18 19:16:04 +0800

    (supabase) Remove unused line in gen-types.ts

--- packages/supabase/gen-types.ts
+++ packages/supabase/gen-types.ts
@@ -1,16 +1,13 @@
-import { exec } from "child_process";
+import { exec } from 'child_process';
 
-// const { PUBLIC_SUPA_PROJ_ID } = Bun.env;
-
-const targetFiles = ["./functions/types.ts"];
-let command =
-	`supabase gen types typescript --local --schema public > ./types.ts;`;
+const targetFiles = ['./functions/types.ts'];
+let command = `supabase gen types typescript --local --schema public > ./types.ts;`;
 
 for (const targetFile of targetFiles) {
 	command += ` cp ./types.ts ${targetFile};`;
 }
 
-command += "rm ./types.ts;";
+command += 'rm ./types.ts;';
 
 exec(command, (err, stdout, stderr) => {
 	if (err) {
