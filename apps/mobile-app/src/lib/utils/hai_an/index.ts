dee13fbcf22eb032dbe600305e0146d17021c49b
davilivob <davilivob@gmail.com>
2024-06-18 19:38:04 +0800

    (mobile app) triggerHaiAn() --> triggerHaiAn(passcode: string)

--- apps/mobile-app/src/lib/utils/hai_an/index.ts
+++ apps/mobile-app/src/lib/utils/hai_an/index.ts
@@ -3,11 +3,11 @@ import { get } from 'svelte/store';
 import { db } from '@/db';
 import { user } from '@/stores';
 
-async function triggerHaiAn() {
+async function triggerHaiAn(passcode: string) {
 	const user_id = get(user)?.id;
 	if (!user_id) return createError('');
 
-	const { data, error } = await db.functions.invoke('hai-an-road', { body: JSON.stringify({ passcode: '81500' }) });
+	const { data, error } = await db.functions.invoke('hai-an-road', { body: JSON.stringify({ passcode }) });
 	await handleEFResponse(error, () => {
 		console.error(error);
 		// const newStatus = get(tripStatus);
