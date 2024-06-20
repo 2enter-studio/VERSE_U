df4f7f4320a3d70f8b755431144dad4d436b0c21
davilivob <davilivob@gmail.com>
2024-06-20 14:06:33 +0800

    (mobile app) Use <Dialog> to render Hai An trigger interface

--- apps/mobile-app/src/routes/side_menu.svelte
+++ apps/mobile-app/src/routes/side_menu.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import { triggerHaiAn } from '@/utils/hai_an';
+	import { Dialog } from '@/components';
+
+	let open = $state(false);
+	let data = $state({ passcode: '' });
+
+	// async function onsubmit(data: { passcode: string }) {
+	// 	await triggerHaiAn(data.passcode);
+	// }
 </script>
 
 <div class="fixed right-0 flex flex-col">
-	<button onclick={triggerHaiAn} class="bg-white p-3 text-black">GO</button>
+	<input type="checkbox" bind:checked={open} />
+	<Dialog title="Hi" {open} class="center-content flex-col text-black">
+<!--		<Form data={{ passcode: '' }} icon="game-icons:teleport" {onsubmit} class="center-content" />-->
+		<input type="text" name="passcode" bind:value={data.passcode} />
+		<button onclick={() => triggerHaiAn(data.passcode)} class="bg-white p-3 text-black">GO</button>
+	</Dialog>
 </div>
