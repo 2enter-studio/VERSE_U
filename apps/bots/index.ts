c021ce66ac653f37c28997dd8825b6b0dd4409c4
davilivob <david.iii.chiu@gmail.com>
2024-06-22 19:43:47 +0800

    (bots) tidy up

--- apps/bots/index.ts
+++ apps/bots/index.ts
@@ -1,22 +1,24 @@
 import { getExpiredTrips, upsertTrips } from './db/trips';
 import { TRIP_UPDATE_TIMEOUT } from './config/index.ts';
 
+// Start auto-tripper
 setInterval(async () => {
   const expiredTrips = await getExpiredTrips();
   if ('error' in expiredTrips) {
     console.error(expiredTrips.error);
     return;
   }
-  // console.log(expiredTrips);
+
   const newTrips = await upsertTrips(expiredTrips);
   if ('error' in newTrips) {
     console.error(newTrips.error);
     return;
   }
+
   if (newTrips.length > 0) {
     console.log(`Successfully updated trips for ${newTrips.length} trips`);
     for (const trip of newTrips) {
-      console.log(trip.id)
+      console.log(trip.id);
     }
   } else {
     console.log(`No expired trips found, do nothing.`);
