8e232dc12bda87165396e80bb456d34bb6f119b2
davilivob <david.iii.chiu@gmail.com>
2024-06-22 20:08:05 +0800

    (supabase) Put genHaiAnPasscode() in utils

--- packages/config/supabase/functions/hai-an-road/index.ts
+++ packages/config/supabase/functions/hai-an-road/index.ts
@@ -1,6 +1,3 @@
-import sha256 from 'sha256';
-import moment from 'moment';
-
 import { corsHeaders } from '../_shared/cors.ts';
 import { validateUser } from '../_shared/db.ts';
 import { createError, createSuccess } from '../_shared/response.ts';
@@ -12,30 +9,30 @@ const key = Deno.env.get('HAI_AN_KEY');
 
 // @ts-ignore
 Deno.serve(async (req) => {
-	// handle preflight request
-	if (req.method === 'OPTIONS') {
-		return new Response('ok', { headers: corsHeaders });
-	}
+  // handle preflight request
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders });
+  }
 
-	// validate user
-	const user = await validateUser(req);
-	if (!user) return createError('invalid auth token');
+  // validate user
+  const user = await validateUser(req);
+  if (!user) return createError('invalid auth token');
 
-	// get passcode from request body
-	const body = await req.json();
-	const passcode = body.passcode as string;
+  // get passcode from request body
+  const body = await req.json();
+  const passcode = body.passcode as string;
 
-	const correctCode = genHaiAnPasscode(key);
-	const success = passcode.toUpperCase() === correctCode;
-	// const success = true;
+  const correctCode = genHaiAnPasscode(key);
+  const success = passcode.toUpperCase() === correctCode;
+  // const success = true;
 
-	if (!success) {
-		return createError(`psych, that's the wrong number, (The right one is: ${correctCode})`);
-	}
+  if (!success) {
+    return createError(`psych, that's the wrong number, (The right one is: ${correctCode})`);
+  }
 
-	const { error } = await admin.from('hai_an_players').insert({
-		player: user.id
-	});
-	if (error) return createError(error.message, { status: 500 });
-	return createSuccess({ message: 'success' });
+  const { error } = await admin.from('hai_an_players').insert({
+    player: user.id
+  });
+  if (error) return createError(error.message, { status: 500 });
+  return createSuccess({ message: 'success' });
 });
