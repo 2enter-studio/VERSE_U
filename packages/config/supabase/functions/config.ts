ec5c0747ce35ca2a7487256ae287ad4abffc90e9
davilivob <david.iii.chiu@gmail.com>
2024-06-22 18:38:20 +0800

    (config) Add new config: MAX_TRAVEL_TIME

--- packages/config/supabase/functions/config.ts
+++ packages/config/supabase/functions/config.ts
@@ -1,8 +1,10 @@
-const MIN_STAY_TIME = 1000 * 3;
-const MAX_STAY_TIME = 1000 * 60 * 60 * 8;
+const MIN_STAY_TIME = 1000 * 5;
+const MAX_STAY_TIME = 1000 * 10;
+const MAX_TRAVEL_TIME = 1000 * 60;
+
 const LOCALES = ['en', 'zh'] as const;
 
 type Locale = (typeof LOCALES)[number];
 
-export { MAX_STAY_TIME, MIN_STAY_TIME, LOCALES };
+export { MAX_STAY_TIME, MIN_STAY_TIME, LOCALES, MAX_TRAVEL_TIME };
 export type { Locale };
