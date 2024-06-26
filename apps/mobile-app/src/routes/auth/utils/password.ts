bfcdd672dc9f9c650b4a3f1e2086e989b51de474
davilivob <david.iii.chiu@gmail.com>
2024-06-26 17:43:18 +0800

    (mobile app) Adjust return value in password.ts

--- apps/mobile-app/src/routes/auth/utils/password.ts
+++ apps/mobile-app/src/routes/auth/utils/password.ts
@@ -5,40 +5,36 @@ import { validate, createError } from '@/utils';
 // Send a password reset email to the given email address
 async function forgotPwd(email: string) {
 	if (!validate.email(email)) {
-		return createError('invalid email');
+		return createError('INVALID_EMAIL');
 	}
 	const { error } = await db.auth.resetPasswordForEmail(email);
-	if (error) return { error };
+	if (error) return createError('OPERATION_FAILED');
 }
 
 // Set a new password for the current auth
 async function setPwd(password: string) {
-	if (!validate.password(password)) return createError('invalid password');
+	if (!validate.password(password)) return createError('INVALID_PASSWORD');
 	const { error } = await db.auth.updateUser({ password });
-	if (error) return { error };
+	if (error) return createError('OPERATION_FAILED');
 
 	{
 		const { error } = await db.auth.getSession();
-		if (error) return { error };
+		if (error) return createError('OPERATION_FAILED');
 	}
 }
 
 // Change the current auth's password after verifying the old password
 async function changePwd(oldPassword: string, newPassword: string) {
 	if (!validate.password(oldPassword) || !validate.password(newPassword))
-		return createError('invalid password');
+		return createError('INVALID_PASSWORD');
 
 	const sessionBackup = authState.session;
 
 	// Return an error if there is no session found
-	if (!sessionBackup) {
-		return createError('you must be logged in to change your password');
-	}
+	if (!sessionBackup) return createError('MUST_LOGIN_FIRST');
 
 	// Return an error if there is no email found
-	if (!sessionBackup.user.email) {
-		return createError('no email found for the current auth, please log out and log back in');
-	}
+	if (!sessionBackup.user.email) return createError('ACCOUNT_NOT_FOUND');
 
 	const { error } = await db.auth.signInWithPassword({
 		email: sessionBackup.user.email,
@@ -48,11 +44,11 @@ async function changePwd(oldPassword: string, newPassword: string) {
 	if (error) {
 		// To prevent auto logout, restore the session
 		authState.session = sessionBackup;
-		return { error };
+		return createError('OPERATION_FAILED');
 	} else {
 		const result = await setPwd(newPassword);
 		if (result?.error) {
-			return { error: result.error };
+			return createError('OPERATION_FAILED');
 		}
 	}
 }
