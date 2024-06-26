00a47d4efbbab287009834aab250a8485b687521
davilivob <david.iii.chiu@gmail.com>
2024-06-26 17:56:04 +0800

    (mobile app) Redirect to /auth/account after sign out

--- apps/mobile-app/src/routes/auth/utils/signout.ts
+++ apps/mobile-app/src/routes/auth/utils/signout.ts
@@ -1,10 +1,10 @@
 import { db } from '@/db';
+import { createError, redirectTo } from '@/utils';
 
 async function signOut() {
 	const { error } = await db.auth.signOut();
-	if (error) return { error };
-	// clearStore();
-	// localStorage.removeItem(`sb-${supaId}-auth-token`);
+	if (error) return createError('OPERATION_FAILED');
+	redirectTo('/auth/account');
 }
 
 export { signOut };
