1be1a6f55849ecb7ca0baec1cf4156c599b7848c
davilivob <david.iii.chiu@gmail.com>
2024-06-27 14:07:01 +0800

    (exhib machine) Fix: wrong HAI_AN_KEY import name

--- apps/exhib-machine/src/routes/api/passcode/+server.ts
+++ apps/exhib-machine/src/routes/api/passcode/+server.ts
@@ -1,11 +1,11 @@
 import { json } from '@sveltejs/kit';
 import sha256 from 'sha256';
 import moment from 'moment';
-import { PASSCODE_KEY } from '$env/static/private';
+import { HAI_AN_KEY } from '$env/static/private';
 
 export const GET = async () => {
 	const now = moment().utc().format('YYYY-MM-DD HH');
-	const passcode = sha256(now + PASSCODE_KEY)
+	const passcode = sha256(now + HAI_AN_KEY)
 		.slice(0, 5)
 		.toUpperCase();
 
