de3db60c798751fed628c27e08361e2ee218458e
davilivob <david.iii.chiu@gmail.com>
2024-06-28 20:10:33 +0800

    (mobile app) Adjust first trip time related values

--- apps/mobile-app/src/routes/auth/utils/profile.ts
+++ apps/mobile-app/src/routes/auth/utils/profile.ts
@@ -25,8 +25,8 @@ async function createProfile(value: { name: string }) {
 	);
 
 	const now = new Date().toISOString();
-	const start_at = addTime(now, -300);
-	const arrive_at = addTime(now, -100);
+	const start_at = addTime(now, -1000 * 60 * 80);
+	const arrive_at = addTime(now, -100 * 60 * 60);
 
 	const { data: tripData, error: tripError } = await db
 		.from('trips')
