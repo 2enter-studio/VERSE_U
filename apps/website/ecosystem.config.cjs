19b7321949661946e1f9f6e3847047a905bccdd5
davilivob <david.iii.chiu@gmail.com>
2024-07-01 21:15:29 +0800

    (website) Update ecosystem.config.cjs

--- apps/website/ecosystem.config.cjs
+++ apps/website/ecosystem.config.cjs
@@ -1,15 +1,23 @@
+require('dotenv/config');
+const PORT = process.env.PORT || 3000;
+const ORIGIN = process.env.ORIGIN || `http://localhost:${PORT}`;
+
 module.exports = {
-	apps: [
-		{
-			name: 'verse_u official website',
-			interpreter: 'bun',
-			script: './build',
-			instances: 1,
-			out_file: 'logs/out.log',
-			error_file: 'logs/error.log',
-			merge_logs: true,
-			log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-			max_memory_restart: '200M'
-		}
-	]
+  apps: [
+    {
+      name: 'VERSE_U official website',
+      script: './build/index.js',
+      exec_mode: 'cluster',
+      instances: 'max',
+      out_file: 'logs/out.log',
+      error_file: 'logs/error.log',
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      max_memory_restart: '200M',
+      env: {
+        PORT,
+        ORIGIN
+      }
+    }
+  ]
 };
