5ea4265d5aa3a36b8a7614341c92b917222f6f83
davilivob <david.iii.chiu@gmail.com>
2024-07-02 16:45:06 +0800

    (mobile app) Implementing From & SubmitBtn to side_menu.svelte

--- apps/mobile-app/src/routes/side_menu.svelte
+++ apps/mobile-app/src/routes/side_menu.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import { triggerHaiAn } from '@/utils/hai_an';
-	import { Dialog } from '@/components';
+	import { Dialog, Form, SubmitBtn } from '@/components';
 
 	let open = $state(false);
 	let data = $state({ passcode: '' });
 </script>
 
-<div class="flex h-[30vh] flex-col bg-white">
+<div class="pointer-events-auto flex h-[30vh] flex-col bg-white">
 	<input type="checkbox" bind:checked={open} />
-	<Dialog title="Hi" {open} class="center-content flex-col text-black">
-		<!--		<Form data={{ passcode: '' }} icon="game-icons:teleport" {onsubmit} class="center-content" />-->
-		<input type="text" name="passcode" bind:value={data.passcode} />
-		<button onclick={() => triggerHaiAn(data.passcode)} class="bg-white p-3 text-black">GO</button>
+	<Dialog title="Hi" bind:open class="center-content flex-col text-black">
+		<Form submitFunction={triggerHaiAn}>
+			<input type="text" name="passcode" />
+			<SubmitBtn>Go</SubmitBtn>
+		</Form>
 	</Dialog>
 </div>
