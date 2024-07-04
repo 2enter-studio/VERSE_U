14d32cbeb45936e5d3609094a943c6295f667660
davilivob <david.iii.chiu@gmail.com>
2024-07-04 12:15:58 +0800

    (mobile app) Init reports.ts

--- apps/mobile-app/src/routes/social/utils/index.ts
+++ apps/mobile-app/src/routes/social/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './chat_info';
 export * from './message';
 export * from './blocks';
+export * from './reports';
---
+++ apps/mobile-app/src/routes/social/utils/reports.ts
@@ -0,0 +1,20 @@
+import validator from 'validator';
+import { handleEFResponse } from '@/utils';
+import { db } from '@/db';
+
+async function reportMessage(args: { message_id: string; reason: string }) {
+	const { message_id, reason } = args;
+	if (!validator.isUUID(message_id)) return;
+
+	const { data, error } = await db.functions.invoke('report-message', {
+		body: JSON.stringify(args)
+	});
+
+	await handleEFResponse(error, () => {
+		console.error(error);
+		// const newStatus = get(tripStatus);
+	});
+	console.log(data);
+}
+
+export { reportMessage };
