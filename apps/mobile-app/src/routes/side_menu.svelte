5da8a1dd11b5fd9353964ce2df65294bb4a36340
davilivob <david.iii.chiu@gmail.com>
2024-07-04 12:32:43 +0800

    (mobile app) Comment out un-finished features in SideMenu && better styling

--- apps/mobile-app/src/routes/side_menu.svelte
+++ apps/mobile-app/src/routes/side_menu.svelte
@@ -10,40 +10,46 @@
 	const items = [
 		{
 			id: 0,
-			name: 'hai_an',
-			icon: 'material-symbols:card-travel-outline-rounded',
-			open: false,
-			class: ''
-		},
-		{
-			id: 1,
 			name: 'social',
-			icon: 'material-symbols:card-travel-outline-rounded',
+			icon: 'fluent:people-48-filled',
 			open: false,
 			class: 'bg-cyan-700'
 		},
-		{ id: 2, name: 'settings', icon: 'mage:settings-fill', class: 'bg-amber-300', open: false }
+		{ id: 1, name: 'settings', icon: 'mage:settings-fill', class: 'bg-gray-300', open: false },
+		// {
+		// 	id: 2,
+		// 	name: 'hai_an',
+		// 	icon: 'material-symbols:card-travel-outline-rounded',
+		// 	open: false,
+		// 	class: 'bg-rose-700'
+		// },
 	] as const;
 </script>
 
-<div class="pointer-events-auto flex flex-col gap-1 rounded-full bg-orange-300 p-1">
+<div
+	class="pointer-events-auto z-[10] flex flex-col gap-1 rounded-full bg-gray-100 p-1 shadow-inner shadow-black/30"
+>
 	{#each items as item}
 		{@const { id, icon, class: className, name } = item}
 		<input id="side_menu_item_{name}" type="checkbox" bind:checked={items[id].open} hidden />
 		<label for="side_menu_item_{name}">
-			<Icon {icon} class="size-8 rounded-full bg-red-800 p-1 {className}" />
+			<Icon {icon} class="size-10 rounded-full p-1.5 shadow-inner shadow-black/40 {className}" />
 		</label>
 	{/each}
 </div>
 
-<Dialog title="Hai An Road" bind:open={items[0].open} class="center-content flex-col text-black">
-	<Form submitFunction={triggerHaiAn}>
-		<input type="text" name="passcode" />
-		<SubmitBtn>Go</SubmitBtn>
-	</Form>
-</Dialog>
+<!--<Dialog title="Hi" bind:open={items[2].open} class="center-content flex-col text-black">-->
+<!--	<Form submitFunction={triggerHaiAn}>-->
+<!--		<input type="text" name="passcode" />-->
+<!--		<SubmitBtn>Go</SubmitBtn>-->
+<!--	</Form>-->
+<!--</Dialog>-->
 
-<Dialog title="Block List" bind:open={items[1].open} class="center-content flex-col text-black">
+<Dialog
+	title={sysState.uiTexts.BLOCK_LIST}
+	bind:open={items[0].open}
+	class="center-content flex-col text-black"
+>
 	{#if gameState.block_users.length !== 0}
 		{#each gameState.block_users as { blocked: id, created_at }}
 			<Form submitFunction={unBlockUser}>
@@ -55,11 +61,12 @@
 			</Form>
 		{/each}
 	{:else}
-		You haven't block anyone
+		{sysState.uiTexts.BLOCK_LIST_EMPTY}
 	{/if}
 </Dialog>
+
 <Dialog
-	bind:open={items[2].open}
+	bind:open={items[1].open}
 	title={sysState.uiTexts.SETTINGS}
 	class="max-h-1/3 center-content flex-col gap-3 text-sm text-black"
 >
