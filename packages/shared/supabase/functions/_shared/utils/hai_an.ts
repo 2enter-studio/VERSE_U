77f5aed41ba9a65fe7056ce94826f63ef91ba0c1
davilivob <david.iii.chiu@gmail.com>
2024-07-05 20:48:19 +0800

    (shared config) set HAI_AN_PASSCODE_DIGIT to 5

--- packages/shared/supabase/functions/_shared/utils/hai_an.ts
+++ packages/shared/supabase/functions/_shared/utils/hai_an.ts
@@ -1,11 +1,12 @@
 import sha256 from 'sha256';
 import moment from 'moment';
+import { HAI_AN_PASSCODE_DIGIT } from '../../config.ts';
 
 // Hai-An road interactive installation related
 function genHaiAnPasscode(key: string) {
   const now = moment.utc().format('YYYY-MM-DD HH');
   return sha256(now + key)
-    .slice(0, 5)
+    .slice(0, HAI_AN_PASSCODE_DIGIT)
     .toUpperCase();
 }
 
--- packages/shared/supabase/functions/config.ts
+++ packages/shared/supabase/functions/config.ts
@@ -1,10 +1,11 @@
 const MIN_STAY_TIME = 1000 * 60 * 10;
 const MAX_STAY_TIME = 1000 * 60 * 60 * 2;
 const MAX_TRAVEL_TIME = 1000 * 60 * 60;
+const HAI_AN_PASSCODE_DIGIT = 5;
 
 const LOCALES = ['en', 'zh'] as const;
 
 type Locale = (typeof LOCALES)[number];
 
-export { MAX_STAY_TIME, MIN_STAY_TIME, LOCALES, MAX_TRAVEL_TIME };
+export { MAX_STAY_TIME, MIN_STAY_TIME, LOCALES, MAX_TRAVEL_TIME, HAI_AN_PASSCODE_DIGIT };
 export type { Locale };
