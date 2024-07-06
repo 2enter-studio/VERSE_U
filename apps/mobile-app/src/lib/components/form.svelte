35bae9613c5683ecab741ea4e104ebaad3f6559f
davilivob <david.iii.chiu@gmail.com>
2024-07-06 17:08:53 +0800

    (mobile app) Update <Form> to allow afterSubmit() as prop

--- apps/mobile-app/src/lib/components/form.svelte
+++ apps/mobile-app/src/lib/components/form.svelte
@@ -2,15 +2,23 @@
 	import type { Snippet } from 'svelte';
 	import { sysState } from '@/states';
 
-	type Props = { children: Snippet; submitFunction: Function; class?: string };
-	let { children, submitFunction, class: className }: Props = $props();
+	type Props<T = any, K = any> = {
+		children: Snippet;
+		submitFunction: (args: T) => Promise<K>;
+		afterSubmit?: (args: K) => Promise<void> | void;
+		class?: string;
+	};
+	let { children, submitFunction, afterSubmit, class: className }: Props = $props();
 	let form = $state<HTMLFormElement>();
 
 	async function onsubmit(e: SubmitEvent) {
 		e.preventDefault();
 		const formData = new FormData(form);
 		const args = Object.fromEntries(formData);
-		await sysState.process(async () => await submitFunction(args));
+		await sysState.process(async () => {
+			const result = await submitFunction(args);
+			await afterSubmit?.(result);
+		});
 	}
 </script>
 
