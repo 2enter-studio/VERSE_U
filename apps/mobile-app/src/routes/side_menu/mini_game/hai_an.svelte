32fc8361df27249de0eaccda796cca053fe593c5
davilivob <david.iii.chiu@gmail.com>
2024-07-08 23:55:56 +0800

    (mobile app) Add Hai-An success animation

--- apps/mobile-app/src/routes/side_menu/mini_game/hai_an.svelte
+++ apps/mobile-app/src/routes/side_menu/mini_game/hai_an.svelte
@@ -1,43 +1,68 @@
 <script lang="ts">
-	import { Form, SubmitBtn } from '@/components';
+	import { Form, SubmitBtn, UModel } from '@/components';
 	import { triggerHaiAn } from '@/utils';
-	import { sysState } from '@/states';
+	import { gameState, sysState } from '@/states';
 	import { HAI_AN_PASSCODE_DIGIT } from '@repo/shared/config';
+	import { FRAME_RATE } from '@/config';
 
 	let passcode = $state<string>('');
 	let success = $state(false);
+	let animating = $state(false);
+	let cameraPosition = $state<[number, number, number]>([0, 7, 2.5]);
+
+	function playAnimation() {
+		animating = true;
+		success = true;
+		let speed = 0.01;
+		const interval = setInterval(() => {
+			cameraPosition[1] -= speed;
+			speed *= 1.35;
+			if (cameraPosition[1] < -100) {
+				clearInterval(interval);
+				animating = false;
+			}
+		}, 1000 / FRAME_RATE);
+	}
 
 	$effect(() => {
 		passcode = passcode.toUpperCase();
 	});
 </script>
 
-<Form
-	submitFunction={triggerHaiAn}
-	afterSubmit={(args) => {
-		if (!args) {
-			success = true;
-			sysState.defaultSuccess('SUCCESS');
-			return;
-		}
-		if ('error' in args) sysState.defaultError('OPERATION_FAILED');
-	}}
-	class="w-[60vw]"
->
+<img
+	src="/images/tube.png"
+	alt="tube"
+	class="{animating
+		? 'top-[-20px]'
+		: 'top-[-50vh]'} absolute z-[120] rotate-180 transition-all duration-500"
+/>
+
+{#if animating}
+	<UModel
+		class="full-screen z-[110] bg-black/50"
+		readonly
+		{cameraPosition}
+		wearingIds={gameState.owned_wearings.filter((w) => w.equipped).map((w) => w.id)}
+	/>
+{/if}
+
+<Form submitFunction={triggerHaiAn} afterSubmit={playAnimation} class="w-[60vw]">
 	<small class="text-xs text-black/80">{sysState.uiTexts.HAI_AN_HINT}</small>
 	<div
 		class="flex h-[60vw] flex-col items-center bg-black bg-cover bg-center bg-no-repeat"
 		style="background-image: url('/images/hai_an_bg.webp')"
 	>
-		<input
-			type="text"
-			id="passcode"
-			name="passcode"
-			maxlength={HAI_AN_PASSCODE_DIGIT}
-			bind:value={passcode}
-			class="w-full bg-purple-900 text-white"
-			placeholder="PASSCODE HERE"
-		/>
+		{#if !success}
+			<input
+				type="text"
+				id="passcode"
+				name="passcode"
+				maxlength={HAI_AN_PASSCODE_DIGIT}
+				bind:value={passcode}
+				class="w-full bg-purple-900 text-white"
+				placeholder="PASSCODE HERE"
+			/>
+		{/if}
 		{#if passcode.trim() !== '' && !sysState.processing && !success}
 			<div class="center-content h-full">
 				<SubmitBtn
