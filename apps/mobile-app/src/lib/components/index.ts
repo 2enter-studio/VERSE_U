c0ff71dc498a43880a2583edd436d747626c0a4e
davilivob <david.iii.chiu@gmail.com>
2024-07-08 16:45:46 +0800

    (mobile app) Remove un-used component

--- apps/mobile-app/src/lib/components/index.ts
+++ apps/mobile-app/src/lib/components/index.ts
@@ -1,8 +1,7 @@
-export { default as Loading } from './loading.svelte';
 export { default as Avatar } from './avatar.svelte';
 export { default as UModel } from './u_model.svelte';
 export { default as Dialog } from './dialog.svelte';
-export { default as Drawer } from './drawer.svelte';
+// export { default as Drawer } from './drawer.svelte';
 export { default as Form } from './form.svelte';
 export { default as SubmitBtn } from './submit_btn.svelte';
 export { default as MenuToggler } from './menu_toggler.svelte';
