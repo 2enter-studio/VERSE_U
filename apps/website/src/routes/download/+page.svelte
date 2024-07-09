5af2a63135fbb9e05928811e8d1aee265510b2c7
davilivob <david.iii.chiu@gmail.com>
2024-07-09 08:25:37 +0800

    (website) Very very very basic /download page

---
+++ apps/website/src/routes/download/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import {APP_LINKS} from '@repo/shared/config';
+</script>
+
+<div class="center-content full-screen flex-col gap-2 text-center">
+  <h1>嗨！請於下方點擊您的作業系統 / 運行環境</h1>
+  <h1>開始體驗 Verse U 吧！</h1>
+  {#each Object.entries(APP_LINKS) as [key, value]}
+    <a href={value} class="text-xl" target="_blank">{key}</a>
+  {/each}
+</div>
