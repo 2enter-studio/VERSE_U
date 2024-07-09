2b86ddf3e1ded572ceaff92558fca0e9571676b0
davilivob <david.iii.chiu@gmail.com>
2024-07-09 22:09:34 +0800

    (exhib machine) Use rendered page instead of GET endpoint for Hai-An passcode

--- apps/exhib-machine/src/routes/api/passcode/+page.server.ts
+++ apps/exhib-machine/src/routes/api/passcode/+page.server.ts
@@ -1,8 +1,7 @@
-import { json } from '@sveltejs/kit';
 import { HAI_AN_KEY } from '$env/static/private';
 import { genHaiAnPasscode } from '@repo/shared/utils';
 
-export const GET = async () => {
+export const load = async () => {
 	const passcode = genHaiAnPasscode(HAI_AN_KEY);
-	return json({ passcode });
+	return { passcode };
 };
---
+++ apps/exhib-machine/src/routes/api/passcode/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { onMount } from 'svelte';
+	import moment from 'moment';
+
+	type Props = { data: PageData };
+	let { data }: Props = $props();
+	const { passcode } = data;
+	onMount(() => {
+		const interval = setInterval(() => {
+			if (moment.utc().format('MM:SS') === '00:00') {
+				window.location.reload();
+			}
+		}, 500);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<div class="flex w-screen h-screen flex-row bg-violet-900 font-bold justify-center items-center">
+	{#each { length: 2 } as _}
+		<div class="text-3xl text-center w-[50vw] text-white">
+			{passcode}
+		</div>
+	{/each}
+</div>
