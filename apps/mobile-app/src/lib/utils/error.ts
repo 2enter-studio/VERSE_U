acd3dfd88ccec02be6d29a742abdd79998db08cf
davilivob <david.iii.chiu@gmail.com>
2024-07-11 23:28:30 +0800

    (mobile app) Return error content in handleEFResponse()

--- apps/mobile-app/src/lib/utils/error.ts
+++ apps/mobile-app/src/lib/utils/error.ts
@@ -14,13 +14,16 @@ async function handleEFResponse(error: any, callback?: Function) {
 	if (error instanceof FunctionsHttpError) {
 		const errorMessage = await error.context.json();
 		console.log('Function returned an error', errorMessage);
+		return errorMessage;
 		// throw Error('OPERATION_FAILED');
 	} else if (error instanceof FunctionsRelayError) {
 		console.log('Relay error:', error.message);
 		// throw Error('OPERATION_FAILED');
+		return error.message;
 	} else if (error instanceof FunctionsFetchError) {
 		console.log('Fetch error:', error.message);
 		// throw Error('OPERATION_FAILED');
+		return error.message;
 	} else {
 		await callback?.();
 	}
