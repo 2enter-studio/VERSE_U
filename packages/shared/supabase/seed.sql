a0c57c99e94fb10a29d73d43aa07d30095cb71e4
davilivob <david.iii.chiu@gmail.com>
2024-07-20 22:29:25 +0800

    (supabase) Insert some regions in seed.sql

--- packages/shared/supabase/seed.sql
+++ packages/shared/supabase/seed.sql
@@ -36,10 +36,18 @@ insert into meshes default values;
 insert into  wearing_types (value)
 values ('wearing_type_0');
 
+insert into regions (x, y) values 
+    (0.5, 0.32),
+    (0.6, 0.43),
+    (0.35, 0.7),
+    (0.68, 0.73),
+    (0.28, 0.55);
+    
+insert into sponsors (value) values ('the_place');
+
 insert into storage.buckets (id, name, public)
 values 
     ('regions', 'regions', true),
     ('wearings', 'wearings', true),
     ('meshes', 'meshes', true),
     ('user_data', 'user_data', false);
-
