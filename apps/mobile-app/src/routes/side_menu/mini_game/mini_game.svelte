e6aa35f42f4a7f3d7346843ae1ca56de00840af9
davilivob <david.iii.chiu@gmail.com>
2024-07-20 23:27:38 +0800

    (mobile app) Add tiny text below some icons

--- apps/mobile-app/src/routes/side_menu/mini_game/mini_game.svelte
+++ apps/mobile-app/src/routes/side_menu/mini_game/mini_game.svelte
@@ -2,6 +2,7 @@
 	import Icon from '@iconify/svelte';
 	import { items, elements } from './config';
 	import { onDestroy } from 'svelte';
+	import { sysState } from '@/states';
 
 	type Props = { selected: keyof typeof elements | null };
 	let { selected = $bindable(null) }: Props = $props();
@@ -10,14 +11,21 @@
 </script>
 
 <div class="center-content w-full flex-row flex-wrap gap-1">
-	{#each items as item}
-		{@const { icon, class: className, name } = item}
-		<input id="mini_game_item_{name}" type="radio" bind:group={selected} value={name} hidden />
-		<label for="mini_game_item_{name}" hidden={!!selected}>
-			<Icon {icon} class="size-10 rounded-full p-1.5 shadow-inner shadow-black/40 {className}" />
-		</label>
-	{/each}
 	{#if selected}
 		<svelte:component this={elements[selected]} />
+	{:else}
+		{#each items as item}
+			{@const { icon, class: className, name } = item}
+			<div class="center-content flex-col">
+				<input id="mini_game_item_{name}" type="radio" bind:group={selected} value={name} hidden />
+				<label for="mini_game_item_{name}" hidden={!!selected}>
+					<Icon
+						{icon}
+						class="size-10 rounded-full p-1.5 shadow-inner shadow-black/40 {className}"
+					/>
+				</label>
+				<small>{sysState.uiTexts[`SIDE_MENU_${name}_TITLE`]}</small>
+			</div>
+		{/each}
 	{/if}
 </div>
